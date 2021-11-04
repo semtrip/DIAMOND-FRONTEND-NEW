59525c52565c00309f3d7cd179a829fb9e1272a2
semtrip <semtrip1@gmail.com>
2021-11-05 00:07:00 +0700

    semtrip

--- src/globals-declare.d.ts
+++ src/globals-declare.d.ts
@@ -1,19 +1,6 @@
 type TTrigger = (eventName: string, ...args: any) => void
 type TFrontTrigger = (triggerName: string, ...args: any) => void
 
-interface IAlt {
-  emit (eventName: string, ...args: any[]): void;
-  off (eventName: string, listener: (...args: any[]) => void): void;
-  on (eventName: string, listener: (...args: any[]) => void): void;
-}
-
-interface IChatAPI {
-  'chat:push': (msg: string) => any
-  'chat:clear': () => any
-  'chat:activate': (active: falce) => any
-  'chat:show': (show: falce) => any
-}
-
 interface IEventManager {
   events: any,
   addHandler: (eventName: string, handler: (...args: any[]) => void) => void
@@ -25,8 +12,6 @@ interface Window {
   frontTrigger: TTrigger
   strTrigger: TTrigger
   trigger: TTrigger
-  chatAPI: IChatAPI
-  alt: IAlt
   test: any
 }
 
@@ -34,6 +19,4 @@ declare var EventManager: IEventManager
 declare var frontTrigger: TTrigger
 declare var strTrigger: TTrigger
 declare var trigger: TTrigger
-declare var chatAPI: IChatAPI
 declare var test: any
-declare var alt: IAlt
--- src/globals.ts
+++ src/globals.ts
@@ -1,14 +1,5 @@
 import './globals-declare.d.ts'
 
-window.frontTrigger = (triggerName: string, ...args: any) => {
-  if (window.alt) {
-    if (args.length) args = args.map((el: any) => {
-      return typeof el === 'object' ? JSON.stringify(el) : el
-    })
-    window.alt.emit(triggerName, ...args)
-  } else console.log(triggerName, ...args)
-}
-
 window.trigger = (eventName: string, ...args: any) => {
   const handlers = EventManager.events[eventName]
   if (!args) handlers.forEach((handler: any) => handler())
@@ -36,12 +27,10 @@ window.EventManager = {
   addHandler (eventName: string, handler: (...args: any[]) => void) {
     if (eventName in this.events) this.events[eventName].push(handler)
     else this.events[eventName] = [handler]
-    if (window.alt) window.alt.on(eventName, handler)
-  },
+      },
   removeHandler (eventName: string, handler: (...args: any[]) => void) {
     if (eventName in this.events) {
       this.events[eventName].splice(this.events[eventName].indexOf(handler), 1)
-      if (window.alt) window.alt.off(eventName, handler)
     }
   }
 }
