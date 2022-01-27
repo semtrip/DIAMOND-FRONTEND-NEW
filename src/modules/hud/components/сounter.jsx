8fe3e84bab4fdb7bc50ee0fab1bc04a8ec661e38
semtrip <75387322+semtrip@users.noreply.github.com>
2022-01-27 20:20:34 +0700

    add style gang

--- src/modules/hud/components/сounter.jsx
+++ src/modules/hud/components/сounter.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { observer } from "mobx-react-lite";
 
 import '../css/counter.css'
+import '../css/counter_ico.css'
+
 
 import store from "../Hud-store";
 
@@ -12,14 +14,14 @@ const state = store.state
 return state.showGang ? (
     <React.Fragment>
         <div className="counter-block">
-            <div className="counter-att">
-                
+            <div className="counter-att vagos">
+                <div className="ico"/>
             </div>
             <div className="counter-timer">
                 {state.timer}
             </div>
-            <div className="counter-def">
-
+            <div className="counter-def ballas">
+                <div className="ico"/>
             </div>
         </div>
     </React.Fragment>
